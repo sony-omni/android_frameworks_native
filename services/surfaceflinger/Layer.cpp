7a563974b2e15b1ce7349a9322eaf10db34ba096
Steve Kondik <steve@cyngn.com>
2014-11-09 13:31:46 -0800

    Add ifdefs for QC-specific dirtyRect feature
    Change-Id: I784e76ead2f8b9eeb6d5e99bd2f83fb6a32db8d8

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -615,6 +615,7 @@ void Layer::setPerFrameData(const sp<const DisplayDevice>& hw,
         layer.setBuffer(mActiveBuffer);
     }
 
+#ifdef QCOM_BSP
     Rect dirtyRect =  mSurfaceFlingerConsumer->getCurrentDirtyRect();
     int bufferOrientation = computeBufferTransform(hw).getOrientation();
     if((mActiveBuffer != NULL) && mTransformHint && !bufferOrientation &&
@@ -660,6 +661,7 @@ void Layer::setPerFrameData(const sp<const DisplayDevice>& hw,
         dirtyRect = resetOrigin.transform(tempDR);
     }
     layer.setDirtyRect(dirtyRect);
+#endif
 
     // NOTE: buffer can be NULL if the client never drew into this
     // layer yet, or if we ran out of memory
